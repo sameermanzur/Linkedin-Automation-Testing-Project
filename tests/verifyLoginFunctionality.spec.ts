f0651ec0f2547142e9656f0d15891add6c5ddfae
sameer Manzur <sameermanzur@icloud.com>
2025-09-01 16:25:03 +1000

    fs and Xlsx added

--- tests/verifyLoginFunctionality.spec.ts
+++ tests/verifyLoginFunctionality.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/loginPage';
 import 'dotenv/config';
 
-test('[DUM-T6] Verify Login', async ({ page }) => {
+test('[T6] Verify Login', async ({ page }) => {
 
   if (!process.env.BASE_URL || !process.env.APP_USERNAME || !process.env.APP_PASSWORD) {
     throw new Error('Missing BASE_URL, APP_USERNAME, or APP_PASSWORD in .env file');
